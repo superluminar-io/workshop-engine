3c92cf4756ce20727bce93f337b2444c69181693
Henrik Fricke <henrikfricke@web.de>
2022-03-23 14:52:47 +0100

    chore: install deps via projen

--- .projenrc.js
+++ .projenrc.js
@@ -3,8 +3,11 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'workshop-engine',
-
-  // deps: [],                /* Runtime dependencies of this module. */
+  deps: [
+    '@aws-cdk/aws-servicecatalog-alpha',
+    '@aws-cdk/aws-appsync-alpha',
+    'cdk-common',
+  ],
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
