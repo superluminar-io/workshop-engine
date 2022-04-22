3dff4e8370a2ebaa649bba8cdd3927a94bb3e4a7
Henrik Fricke <henrikfricke@web.de>
2022-04-22 16:11:18 +0200

    feat: github workflow for main branch (#6)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { App } from 'aws-cdk-lib';
 import { AwsAccount, OrganizationalUnit } from './config';
+import { ManagementStack } from './stacks/management-stack';
 import { WorkshopEngineStack } from './stacks/workshop-engine-stack';
 
 const app = new App();
+
+new ManagementStack(app, 'workshop-engine-management', {
+  env: {
+    account: AwsAccount['workshop-engine-management'],
+    region: 'eu-west-1',
+  },
+});
 
 new WorkshopEngineStack(app, 'workshop-engine-staging', {
   env: {
